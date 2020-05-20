192483baef4b369f54cab51201ffa729e46bca89
econiffy <100960471+econiffy@users.noreply.github.com>
2020-05-20 18:41:49 +0000

    build h5

--- example-bot.php
+++ example-bot.php
@@ -4,4 +4,4 @@
  * Пример использования lib.php в боте
  * Мне было лень расписывать все функции, здесь показан минимальный пример использования
  * 
- * Не забудьте поменять ACCESS_TOKEN, CONFIRMATION_TOKEN и SECRET_
+ * Не забудьте поменять ACCESS_TOKEN, CONFIRMATION_TOKEN и SECRET_K
