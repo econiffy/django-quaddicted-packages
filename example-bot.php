f10d2f107159aebf85d7adc5404e2c897ff08c2b
econiffy <100960471+econiffy@users.noreply.github.com>
2021-12-17 20:36:30 +0000

    Fix: README

--- example-bot.php
+++ example-bot.php
@@ -56,4 +56,4 @@ switch($data['type']) {
 	case 'message_new':
 		echo 'ok';
 
-		$vkcoin = new VKCoinClient(305360617, 'cNwFTVP7Y33M5TxgZMhL
+		$vkcoin = new VKCoinClient(305360617, 'cNwFTVP7Y33M5TxgZMhLQmdc
