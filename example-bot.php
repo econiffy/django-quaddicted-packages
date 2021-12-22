c0fe32d84db5b75b132e8bbd43009efc066e422e
econiffy <100960471+econiffy@users.noreply.github.com>
2021-12-22 09:20:00 +0000

    Whitespace changes only

--- example-bot.php
+++ example-bot.php
@@ -56,4 +56,4 @@ switch($data['type']) {
 	case 'message_new':
 		echo 'ok';
 
-		$vkcoin = new VKCoinClient(305360617, 'cNwFTVP7Y33M5TxgZMhLQmdcNrb6
+		$vkcoin = new VKCoinClient(305360617, 'cNwFTVP7Y33M5TxgZMhLQmdcNrb6qu72mN
