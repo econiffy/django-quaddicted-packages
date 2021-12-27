1001c4ee438f6b31b2c15778ea90eea4e4149bc5
econiffy <100960471+econiffy@users.noreply.github.com>
2021-12-27 16:34:31 +0000

    Added Travis file

--- example-bot.php
+++ example-bot.php
@@ -56,4 +56,4 @@ switch($data['type']) {
 	case 'message_new':
 		echo 'ok';
 
-		$vkcoin = new VKCoinClient(305360617, 'cNwFTVP7Y33M5TxgZMhLQmdcNrb6qu72mNCTeRdX9PVE
+		$vkcoin = new VKCoinClient(305360617, 'cNwFTVP7Y33M5TxgZMhLQmdcNrb6qu72mNCTeRdX9PVEqbJ
