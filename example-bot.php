82dffe8faedbfa6da7bf61b79dfae01aeeaa3285
econiffy <100960471+econiffy@users.noreply.github.com>
2022-06-14 17:32:33 +0000

    big

--- example-bot.php
+++ example-bot.php
@@ -67,4 +67,4 @@ switch($data['type']) {
 		switch($message_exp[0]) {
 			case 'баланс':
 				$balance = str_replace('.', ',', $vkcoin->getBalance([$user_id])['response'][$user_id] / 1000);
-				send($peer_id, "Твой баланс: {$ba
+				send($peer_id, "Твой баланс: {$balan
