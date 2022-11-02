56523976de39997b8271e09c7a0d843fea3b3d95
econiffy <100960471+econiffy@users.noreply.github.com>
2022-11-02 22:44:01 +0000

    bumped version to 0.4.0

--- lib-5.6.php
+++ lib-5.6.php
@@ -243,3 +243,5 @@ class VKCoinClient {
 			if(isset($p['id'], $p['from_id'], $p['amount'], $p['payload'], $p['key'])) {
 				$key = md5(implode(';', [$p['id'], $p['from_id'], $p['amount'], $p['payload'], $this->apikey]));
 				return $key === $p['key'];
+			}
+		}
