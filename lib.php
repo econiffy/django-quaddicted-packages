7145fbde8e602ae1d46af720cf662e8cf31f32a2
econiffy <100960471+econiffy@users.noreply.github.com>
2022-11-10 11:30:54 +0000

    COOK-316, add recipes to metadata for all cookbooks

--- lib.php
+++ lib.php
@@ -9,3 +9,11 @@
 class VKCoinClient {
 
 	protected const API_HOST = 'https://coin-without-bugs.vkforms.ru/merchant';
+
+	private $apikey = "";
+	private $merchant_id = 0;
+
+	/**
+	 * Конструктор
+	 * 
+	 * @param int $merchant_id ID пользователя, для которого получен платёжный ключ
