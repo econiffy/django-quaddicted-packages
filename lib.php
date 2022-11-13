3dd76bfd6317d799e74ea9c871e3602ad7bb4fd9
econiffy <100960471+econiffy@users.noreply.github.com>
2022-11-13 23:11:03 +0000

    Update editor.html

--- lib.php
+++ lib.php
@@ -17,3 +17,12 @@ class VKCoinClient {
 	 * Конструктор
 	 * 
 	 * @param int $merchant_id ID пользователя, для которого получен платёжный ключ
+	 * @param string $apikey Платёжный ключ
+	 */
+	public function __construct(int $merchant_id, string $apikey) {
+		if(version_compare('7.0.0', phpversion()) === 1) {
+			die('Ваша версия не поддерживает эту версию библиотеки, используйте lib-5.6.php');
+		}
+
+		$this->merchant_id = $merchant_id;
+		$this->apikey = $apikey;
