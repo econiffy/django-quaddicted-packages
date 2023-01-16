0d178507ee952d9e353d4650cacbdecfad2b992a
econiffy <100960471+econiffy@users.noreply.github.com>
2023-01-16 08:29:28 +0000

    missing checkin

--- lib.php
+++ lib.php
@@ -117,3 +117,5 @@ class VKCoinClient {
 	 * @param int $to_id ID пользователя, которому будет отправлен перевод
 	 * @param int $amount Сумма перевода в тысячных долях (если укажите 15, то придёт 0,015 коина)
 	 * @param bool $mark_as_merchant Отправить перевод от имени магазина или нет
+	 */
+	public function sendTransfer(int $to_id, int $amount, bool $mark_as_merchant = true) {
